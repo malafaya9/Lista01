682776b539c4f25680dfcb5e00478f76cde7697c
Vinicius Albuquerque Malafaya <malafaya9@gmail.com>
2017-08-17 18:10:27 -0300

    Questão 3 Parcial

---
+++ Q3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+float exp(float);
+
+int main()
+{
+    return 0;
+}
+
+float exp(float n){
+    float p, t;
+    t = 0;
+    do{
+        t+=p;
+    }while(p>pow(10,-6));
+
+    return n;
+}
