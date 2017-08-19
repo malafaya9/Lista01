4fb08b67aef5ceecd999052db5733db5c7bff660
Vinicius Albuquerque Malafaya <malafaya9@gmail.com>
2017-08-19 17:23:17 -0300

    Questão 3 Adicionada

--- Q3.cpp
+++ Q3.cpp
@@ -3,19 +3,39 @@
 
 using namespace std;
 
-float exp(float);
+double my_exp(double);
+
+int fat(int);
 
 int main()
 {
+    cout << "Insira um numero: ";
+    int num;
+    cin >> num;
+    cout << "Valor da exponencial por my_exp: " << my_exp(num) << endl
+         << "Valor da exponencial por    exp: " << exp(num);
     return 0;
 }
 
-float exp(float n){
-    float p, t;
-    t = 0;
+double my_exp(double n){
+    double p = 0, t = 1;
+    int i = 0;
     do{
         t+=p;
-    }while(p>pow(10,-6));
+        i++;
+        p = (1.0*pow(n, i))/fat(i);
+    }while(p>10e-6);
 
-    return n;
+    return t;
+}
+
+int fat(int n){
+    if(n==0){
+            return 0;
+    }
+    int r = 1;
+    for(int i = 1; i <= n; i++){
+        r*=i;
+    }
+    return r;
 }
